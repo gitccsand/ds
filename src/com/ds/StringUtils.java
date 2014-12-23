d4874b7464565a956892d318d463779840929275
gitccsand <gozilla@163.com>
2014-12-23 11:25:46 +0800

    httponly cookie

---
+++ src/com/ds/StringUtils.java
@@ -0,0 +1,16 @@
+package com.ds;
+
+
+public class StringUtils {
+	
+	public static boolean validateNull(String str){
+		if(str==null)return true;
+		if(str.isEmpty())return true;
+		return false;
+	}
+	public static String filterHtml(String str){
+		
+		return str;
+	}
+
+}
